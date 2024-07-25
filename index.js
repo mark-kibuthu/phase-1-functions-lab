fd4ccd093fe47b98dd3b681bccddaaa29755d995
Agent-grey <markkibuthu@gmail.com>
2024-07-25 10:20:45 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(pickupLocation) {
+    return Math.abs(pickupLocation - 42);
+}
+
+function distanceFromHqInFeet(pickupLocation) {
+    let blocks = distanceFromHqInBlocks(pickupLocation);
+    return blocks * 264;
+}
+
+function distanceTravelledInFeet(start, destination) {
+    let blocksTravelled = Math.abs(destination - start);
+    return blocksTravelled * 264;
+}
+function calculatesFarePrice(start, destination) {
+    const feetTravelled = distanceTravelledInFeet(start, destination);
+
+    if (feetTravelled <= 400) {
+        return 0;
+    } else if (feetTravelled > 400 && feetTravelled <= 2000) {
+        return (feetTravelled - 400) * 0.02;
+    } else if (feetTravelled > 2000 && feetTravelled <= 2500) {
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+}
+
+module.exports = {
+    distanceFromHqInBlocks,
+    distanceFromHqInFeet,
+    distanceTravelledInFeet,
+    calculatesFarePrice
+};
